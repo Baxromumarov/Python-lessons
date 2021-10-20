cfc8c7789c7428516996a858ab48edf5deea13e3
Baxrom <92705188+Baxromumarov@users.noreply.github.com>
2021-10-20 10:24:14 +0500

    Dasturlar yangilandi!
    Yangi qismlari qo'shildi

---
+++ dictionary lesson.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 09:00:56 2021
+
+@author: User
+"""
+car={'model':'sonata','narx':'500 mln','rang':'qora','chiq_yil':'2020'}
+# print('Rusumi-',car['model'].title())
+# print('Rangi-',car['rang'])
+# print('Narxi-',car['narx'])
+# print('Chiqqan yili',car['chiq_yil'])
+
+
+# talaba={'ism':'baxrom','yosh':'17','kurs':'1'}
+# print(f"{talaba['ism'].title()},\
+# {2021-int(talaba['yosh'])}-yilda tug\'ilgan,\
+# hozirda {talaba['kurs']}-kurs talabasi")
+
+
+
+
+# car={
+#      'company':'hyundai',
+#      'model':'sonata',
+#      'color':'black',
+#      'year':'2020',
+#      'gear':'automatic',
+#      'price':'500 mln'
+#      }
+# print(car['model'].title())
+# model=car.get('model','Bunday kalit mavjud emas')
+# print(model.title())
+
+
+
+
+# talaba['kurs']=2
+# talaba['yo\'nalish']='avtomobil servisi'
+# print(talaba)
+
+
+
+# cars={}
+# cars['model']='k5'
+# cars['company']='kia'
+# cars['price']=20_000
+# cars['color']='black'
+# cars['price']=23_000
+# print(f"{cars['company'].title()} tomonidan ishlab chiqailgan,\
+# Rusumi: {cars['model'].title()},\
+# Narxi: { cars['price']} $,\
+# Rangi: { cars['color']}")
+
+# del cars['color']
+# print(cars)
+
